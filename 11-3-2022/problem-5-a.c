25b0d0675fed70dad03a668f9bc09de338d7d3cc
Axay Patoliya <axay.patoliya@einfochips.com>
2022-03-15 14:23:43 +0530

    added

---
+++ 11-3-2022/problem-5-a.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    int pid, fd;
+
+    pid = fork();
+    if(pid==0){
+        // run this as s child process
+        fd = open("linux400.txt", O_CREAT | O_RDWR, 777);
+        char to_write[] = "this lines will be added to file. this one is also be added";
+        int leng = strlen(to_write);
+        char to_read[leng];
+        int i;
+        if (fd>0){
+            if((i = write(fd, to_write, leng)) < 0){
+                perror("error occured while writing to a file");
+            }
+        }
+        read(fd, to_read, leng);
+    } else{
+        // run this as a parent process
+        wait(0);
+        printf("parent process\n");
+    }
+    return 0;
+}
